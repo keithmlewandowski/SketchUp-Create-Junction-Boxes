093359b325601aba0f429d162cf5112964b47697
Keith Lewandowski <101996013+keithmlewandowski@users.noreply.github.com>
2024-09-03 14:57:14 -0400

    Code optimization

--- create_junction_box/main.rb
+++ create_junction_box/main.rb
@@ -4,25 +4,31 @@ require 'csv'
 #Namespace
 module JunctionBox 
 
+# Function to extrude any size rectangle in the vertical direction
+def JunctionBox.extrudeRect(x1, y1, x2, y2, z, extrudeDepth, rectGroup)
+  points = [
+    Geom::Point3d.new(x1,y1,z),
+    Geom::Point3d.new(x2,y1,z),
+    Geom::Point3d.new(x2,y2,z),
+    Geom::Point3d.new(x1,y2,z)
+  ]
+  face = rectGroup.entities.add_face(points)
+  face.pushpull(extrudeDepth)
+end
+
+# Main junction box creation function
 def JunctionBox.main(fileDir, jbName, iterationOffset, baseWidth, baseDepth, baseHeight, boxWidth, boxDepth, boxHeight, boxThickness, lidHoleWidth, lidHoleDepth, lidHeight, holeValues)
 
   # Make Base
   model = Sketchup.active_model
   model.start_operation('Create JB Box', true)
 
-  group = model.active_entities.add_group
-  group.name = jbName
-  entities = group.entities  
-  box = entities.add_group
-  if baseWidth != 0 && baseDepth != 0 && baseHeight != 0   ### -- TODO: Deal with blank cells -- ###
-    points = [
-      Geom::Point3d.new(0 + iterationOffset,0,0),
-      Geom::Point3d.new(baseWidth + iterationOffset,0,0),
-      Geom::Point3d.new(baseWidth + iterationOffset,baseDepth,0),
-      Geom::Point3d.new(0 + iterationOffset,baseDepth,0)
-    ]
-    face = box.entities.add_face(points)
-    face.pushpull(-baseHeight)
+  $group = model.active_entities.add_group
+  $group.name = jbName
+  entities = $group.entities  
+  $box = entities.add_group
+  if baseWidth != 0 && baseDepth != 0 && baseHeight != 0  
+    extrudeRect(iterationOffset, 0, baseWidth + iterationOffset, baseDepth, 0, -baseHeight, $box)
   else
     baseWidth = boxWidth
     baseDepth = boxDepth
@@ -30,31 +36,18 @@ def JunctionBox.main(fileDir, jbName, iterationOffset, baseWidth, baseDepth, bas
   end
 
   # Make Box 
-  if boxWidth != 0 && boxDepth != 0 && boxHeight != 0   ### -- TODO: Deal with blank cells -- ###
+  if boxWidth != 0 && boxDepth != 0 && boxHeight != 0  
 
     widthOffset = (baseWidth-boxWidth) / 2
     depthOffset = (baseDepth-boxDepth) / 2
-
-    points = [
-      Geom::Point3d.new(widthOffset + iterationOffset,depthOffset,baseHeight),
-      Geom::Point3d.new(widthOffset + boxWidth + iterationOffset,depthOffset,baseHeight),
-      Geom::Point3d.new(widthOffset + boxWidth + iterationOffset,depthOffset + boxDepth,baseHeight),
-      Geom::Point3d.new(widthOffset + iterationOffset,depthOffset + boxDepth,baseHeight)
-    ]
-    face = box.entities.add_face(points)
-    face.pushpull(boxHeight)
-
-    points = [
-      Geom::Point3d.new(widthOffset + boxThickness + iterationOffset,depthOffset + boxThickness,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + boxWidth - boxThickness + iterationOffset,depthOffset + boxThickness,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + boxWidth - boxThickness + iterationOffset,depthOffset + boxDepth - boxThickness,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + boxThickness + iterationOffset,depthOffset + boxDepth - boxThickness,baseHeight + boxHeight)
-    ]
-    face = box.entities.add_face(points)
-    face.pushpull(-boxHeight)
     
+    # Box outer rectangle
+    extrudeRect(widthOffset + iterationOffset,depthOffset,widthOffset + boxWidth + iterationOffset,depthOffset + boxDepth,baseHeight,boxHeight, $box)
 
-    # Add Pipe Holes (first half)
+    # Remove box inner rectangle
+    extrudeRect(widthOffset + boxThickness + iterationOffset,depthOffset + boxThickness,widthOffset + boxWidth - boxThickness + iterationOffset,depthOffset + boxDepth - boxThickness,baseHeight + boxHeight,-boxHeight, $box)
+
+    # Add Pipe Holes
     hole_punch = entities.add_group
     for i in 0..(holeValues.length - 1) do    
 
@@ -89,76 +82,54 @@ def JunctionBox.main(fileDir, jbName, iterationOffset, baseWidth, baseDepth, bas
 
       normal_vector = Geom::Vector3d.new(Math.sin(theta),Math.cos(theta),0)
       radius = (holeHeight / 2)
-
+      
+      # Create solid pipes
       hole_outer = hole_punch.entities.add_circle(center_point, normal_vector, radius)
       face = hole_punch.entities.add_face(hole_outer)
       face.pushpull(4 * boxThickness)
-
-      
     end
-    hole_punch.subtract(box)
-    model.commit_operation    
-  
+    # Remove pipe hole from box
+    hole_punch.subtract($box)  
   end
     
-    
 
-  
   # Make Lid
   if lidHoleWidth != 0 && lidHoleDepth != 0 && lidHeight != 0   ### -- TODO: Deal with blank cells -- ###
+    model.commit_operation
     model = Sketchup.active_model
     model.start_operation('Create JB Lid', true)
-
-    #group = model.active_entities.add_group
-    #entities = group.entities  
+ 
     widthOffset = (baseWidth-boxWidth) / 2
     depthOffset = (baseDepth-boxDepth) / 2
-    points = [
-      Geom::Point3d.new(widthOffset + iterationOffset,depthOffset,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + boxWidth + iterationOffset,depthOffset,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + boxWidth + iterationOffset,depthOffset + boxDepth,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + iterationOffset,depthOffset + boxDepth,baseHeight + boxHeight)
-    ]
-    face = entities.add_face(points)
-    face.pushpull(lidHeight)
+
+    # full lid rectangle
+    extrudeRect(widthOffset + iterationOffset,depthOffset,widthOffset + boxWidth + iterationOffset,depthOffset + boxDepth,baseHeight + boxHeight,lidHeight,$group)
+
     lidWidthOffset = ((boxWidth - (2 * boxThickness)) - lidHoleWidth) / 2
     lidDepthOffset = ((boxDepth - (2 * boxThickness)) - lidHoleDepth)
 
-    points = [
-      Geom::Point3d.new(widthOffset + boxThickness + lidWidthOffset + iterationOffset,depthOffset + boxThickness + lidDepthOffset,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + boxWidth - boxThickness - lidWidthOffset + iterationOffset,depthOffset + boxThickness + lidDepthOffset,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + boxWidth - boxThickness - lidWidthOffset + iterationOffset,depthOffset + boxDepth - boxThickness,baseHeight + boxHeight),
-      Geom::Point3d.new(widthOffset + boxThickness + lidWidthOffset + iterationOffset,depthOffset + boxDepth - boxThickness,baseHeight + boxHeight)
-    ]
-
-    face = entities.add_face(points)
-    face.pushpull(-lidHeight)
-    #comp = model.entities.add_group(group).to_component
-    #comp.name = jbName + "_Lid"
-    #compPath = File.join(fileDir, jbName + "_Lid" + ".skp")
-    #compdef = comp.definition
-    #compdef.save_as(compPath)
-    model.commit_operation  
+    # remove lid hole
+    extrudeRect(widthOffset + boxThickness + lidWidthOffset + iterationOffset,depthOffset + boxThickness + lidDepthOffset,widthOffset + boxWidth - boxThickness - lidWidthOffset + iterationOffset,depthOffset + boxDepth - boxThickness,baseHeight + boxHeight, -lidHeight, $group)
   end
-  comp = model.entities.add_group(group).to_component
+
+  # create and save component
+  comp = model.entities.add_group($group).to_component
   comp.name = jbName
   compPath = File.join(fileDir, jbName + ".skp")
   compdef = comp.definition
   compdef.save_as(compPath)
-
+  model.commit_operation  
 end
-
 
 toolbar = UI::Toolbar.new "Junction Box"
 cmd = UI::Command.new("Batch Create CSV") {
-
-
+# Select and read file
 file = UI.openpanel('CSV File', 'c:\\')
+#fileFormat = UI.inputbox(["Which format is your file in? \n1: \n2: \n3:"])
 table = CSV.read(file)
 boxNum = table.length
 iterationOffset = 0
-fileFormat = UI.inputbox(["Which format is your file in? \n1: \n2: \n3:"])
-
+# Create component save directory
 pathName = file.to_s
 basename = File.basename(file, ".csv").to_s
 pathName.slice!(File.basename(file).to_s)
@@ -167,8 +138,9 @@ unless File.directory?(fileDir)
   Dir.mkdir(fileDir)
 end
 
+# Create each box
 for i in 1..boxNum do
-
+  # Identify variables
   jbName = table[i][0]
   baseWidth = table[i][1].to_f
   baseDepth = table[i][2].to_f
@@ -195,12 +167,16 @@ for i in 1..boxNum do
       holeValues = [[table[i][11+(n*4)].to_f, table[i][12+(n*4)].to_f, table[i][13+(n*4)].to_f, table[i][14+(n*4)].to_f]].unshift(*holeValues)
     end
   end
+
+  # Make junction box
   main(fileDir, jbName, iterationOffset, baseWidth, baseDepth, baseHeight, boxWidth, boxDepth, boxHeight, boxThickness, lidHoleWidth, lidHoleDepth, lidHeight, holeValues)
+  
   iterationOffset = iterationOffset + baseWidth + 36
 
 end
 }
 
+# Define toolbar button
 icon = File.join(__dir__, 'icon.png')
 cmd.small_icon = icon
 cmd.large_icon = icon
